2ba2fb201e57dba51ed2883d2ee32271b25a7096
Aren Chen <yarencheng@gmail.com>
2018-05-30 21:33:33 +0800

    start docker service

--- ubuntu16.04/entry.sh
+++ ubuntu16.04/entry.sh
@@ -3,11 +3,9 @@
 UID=`id -u`
 GID=`id -g`
 
-#addgroup --gid $GID jenkins
-#adduser -q --uid $UID --gid $GID --disabled-password --gecos "" jenkins
-
 echo 'jenkins:x:'$GID':' >> /etc/group
 echo 'jenkins:x:'$UID':'$GID':,,,:/home/jenkins:/bin/bash' >> /etc/passwd
 
+sudo service docker start
 
 bash
